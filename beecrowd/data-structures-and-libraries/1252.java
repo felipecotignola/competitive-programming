8273c75ebe77852a9819eb2ce939a2f84ee3ce46
Felipe Cotignola <fsa398975@gmail.com>
2026-08-27 12:27:01 -0300

    att

--- beecrowd/data-structures-and-libraries/1252.java
+++ beecrowd/data-structures-and-libraries/1252.java
@@ -1,32 +1,49 @@
 import java.io.IOException;
 import java.util.Scanner;
-/**
- * IMPORTANT: 
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
+
 public class Main {
- 
     public static void main(String[] args) throws IOException {
-        Scanner sc=new Scanner(System.in);
-        int t=sc.nextInt();
-        for(int i=0;i<t;i++){
-            int n=sc.nextInt(),m=sc.nextInt();
-            int[] vet=new int[n];
-            for(int i=0;i<n;i++){
-                vet[i]=sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        while (n != 0 || m != 0) {
+            int[] array = new int[n];
+            for (int j = 0; j < n; j++) {
+                array[j] = sc.nextInt();
             }
-            for(int i=0;i<n-1;i++){
-                int smallest=i;
-                for(int j=i+1;j<n;j++){
-                    if(vet[j]%m<vet[smallest]%m){
-                        
+            for (int k = 0; k < n - 1; k++) {
+                int smallest = k;
+                for (int l = k + 1; l < n; l++) {
+                    if (array[l] % m < array[smallest] % m) {
+                        smallest = l;
+                    }
+                    else if (array[l] % m == array[smallest] % m) {
+                        if (array[l] % 2 != 0 && array[smallest] % 2 != 0) {
+                            if (array[l] > array[smallest]) {
+                                smallest = l;
+                            }
+                        }
+                        else if (array[l] % 2 == 0 && array[smallest] % 2 == 0) {
+                            if (array[l] < array[smallest]) {
+                                smallest = l;
+                            }
+                        }
+                        else if (array[l] % 2 != 0 && array[smallest] % 2 == 0) {
+                            smallest = l;
+                        }
                     }
                 }
+                int temp = array[k];
+                array[k] = array[smallest];
+                array[smallest] = temp;
             }
+            System.out.printf("%d %d\n", n, m);
+            for (int p = 0; p < n; p++) {
+                System.out.printf("%d\n", array[p]);
+            }
+            n = sc.nextInt();
+            m = sc.nextInt();
         }
- 
+        System.out.println("0 0");
     }
- 
 }
