f046b2330225727d7964b702e2a5f7b720cfa03b
Felipe Cotignola <fsa398975@gmail.com>
2026-08-27 15:55:06 -0300

    att

--- beecrowd/data-structures-and-libraries/1252.java
+++ beecrowd/data-structures-and-libraries/1252.java
@@ -1,49 +1,52 @@
 import java.io.IOException;
 import java.util.Scanner;
-
+ 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        while (n != 0 || m != 0) {
-            int[] array = new int[n];
-            for (int j = 0; j < n; j++) {
-                array[j] = sc.nextInt();
-            }
-            for (int k = 0; k < n - 1; k++) {
-                int smallest = k;
-                for (int l = k + 1; l < n; l++) {
-                    if (array[l] % m < array[smallest] % m) {
-                        smallest = l;
-                    }
-                    else if (array[l] % m == array[smallest] % m) {
-                        if (array[l] % 2 != 0 && array[smallest] % 2 != 0) {
-                            if (array[l] > array[smallest]) {
-                                smallest = l;
-                            }
-                        }
-                        else if (array[l] % 2 == 0 && array[smallest] % 2 == 0) {
-                            if (array[l] < array[smallest]) {
-                                smallest = l;
-                            }
-                        }
-                        else if (array[l] % 2 != 0 && array[smallest] % 2 == 0) {
-                            smallest = l;
-                        }
-                    }
-                }
-                int temp = array[k];
-                array[k] = array[smallest];
-                array[smallest] = temp;
-            }
-            System.out.printf("%d %d\n", n, m);
-            for (int p = 0; p < n; p++) {
-                System.out.printf("%d\n", array[p]);
-            }
-            n = sc.nextInt();
-            m = sc.nextInt();
+    static boolean check(int num,int chave,int m){
+        if(chave%m!=num%m){
+            return chave<num;
         }
-        System.out.println("0 0");
+        else if(chave%2==0 && num%2==0){
+            return chave>num;
+        }
+        else if(chave%2!=0 && num%2!=0){
+            return chave<num;
+        }
+        else{
+            if(chave%2==0){
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
     }
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(),m=sc.nextInt();
+        while(n!=0 && m!=0){
+            int[] array=new int[n];
+            for(int i=0;i<n;i++){
+                array[i]=sc.nextInt();
+            }
+            for(int i=1;i<n;i++){
+                int chave=array[i];
+                int k=i-1;
+                while(k>=0 && check(array[k],chave,m)){
+                    array[k+1]=array[k];
+                    k--;
+                }
+                array[k+1]=chave;
+            }
+            System.out.printf("%d %d\n",n,m);
+            for(int i=0;i<n;i++){
+                System.out.println(array[i]);
+            }
+            n=sc.nextInt();
+            m=sc.nextInt();
+        }
+        System.out.printf("%d %d",n,m);
+    }
+ 
 }
