e23542be70b78b3939c7072131f5c78985478167
Felipe <fsa398975@gmail.com>
2026-09-03 15:16:13 -0300

    att

--- beecrowd/data-structures-and-libraries/1110.c
+++ beecrowd/data-structures-and-libraries/1110.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
+
 typedef struct{
     int* remainingCards;
     int* discardedCards;
     int top,bottom,quantity,capacity,discartedIndex;
 }Deck;
+
 void fill(Deck* deck){
     for(int i=1;i<=(*deck).capacity;i++){
         (*deck).remainingCards[++(*deck).bottom]=i;
     }
 }
+
 void set(Deck* deck,int n){
     (*deck).remainingCards=malloc(n*sizeof(int));
     (*deck).discardedCards=malloc((n-1)*sizeof(int));
@@ -20,19 +23,24 @@ void set(Deck* deck,int n){
     (*deck).discartedIndex=0;
     fill(deck);
 }
+
 int remov(Deck* deck){
-    int resp=(*deck).remainingCards[(*deck).top++%(*deck).capacity];
+    int resp=(*deck).remainingCards[(*deck).top];
+    (*deck).top=((*deck).top+1)%(*deck).capacity;
     (*deck).quantity--;
-    (*deck).remainingCards[++(*deck).bottom%(*deck).capacity]=(*deck).remainingCards[(*deck).top];
-    (*deck).top=++(*deck).top%(*deck).capacity;
+    (*deck).bottom=((*deck).bottom+1)%(*deck).capacity;
+    (*deck).remainingCards[(*deck).bottom]=(*deck).remainingCards[(*deck).top];
+    (*deck).top=((*deck).top+1)%(*deck).capacity;
     return resp;
 }
+
 void loop(Deck* deck){
     while((*deck).quantity>=2){
         (*deck).discardedCards[(*deck).discartedIndex]=remov(deck);
         (*deck).discartedIndex++;
     }
 }
+
 void print(Deck* deck){
     printf("Discarded cards: ");
     for(int i=0;i<(*deck).discartedIndex;i++){
@@ -45,9 +53,11 @@ void print(Deck* deck){
     }
     printf("Remaining card: %d\n",(*deck).remainingCards[(*deck).top]);
 }
+
 int main() {
     int n;
     scanf("%d",&n);
+
     while(n!=0){
         Deck deck;
         set(&deck,n);
@@ -55,5 +65,6 @@ int main() {
         print(&deck);
         scanf("%d",&n);
     }
+
     return 0;
 }
